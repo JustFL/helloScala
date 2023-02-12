b5899df360993786ff1a0f96ca45e57fb1526889
Prometheus <598681353@qq.com>
2023-02-12 16:20:58 +0800

    add comment

---
+++ src/main/scala/Operator.scala
@@ -0,0 +1,25 @@
+object Operator {
+  def main(args: Array[String]): Unit = {
+
+    // ==更加类似于Java中的equals
+    val s1 = "abc"
+    val s2 = new String("abc")
+
+    println(s1 == s2)
+    println(s1.eq(s2))
+
+
+    // 在Scala中其实是没有运算符的 所有运算符都是方法
+    // 标准的加法运算
+    val i: Int = 1.+(1)
+    // 当调用对象的方法时 .可以省略
+    val j: Int = 1 + (1)
+    // 如果函数参数只有一个或者没有参数 ()可以省略
+    val k: Int = 1 + 1
+    println(i + "-" + j + "-" + k)
+
+    println(1.toString())
+    println(1 toString())
+    println(1 toString)
+  }
+}
