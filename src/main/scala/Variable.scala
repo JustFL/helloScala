e753e1da285396fd35b163ae3460c3ff4e5d3d23
Prometheus <598681353@qq.com>
2023-02-12 15:44:51 +0800

    add null explain

--- src/main/scala/Variable.scala
+++ src/main/scala/Variable.scala
@@ -1,8 +1,18 @@
 object Variable {
   def main(args: Array[String]): Unit = {
 
-    // unit类型表示没有返回值 Unit只有一个实例值()
+    // Unit类型表示没有返回值 Unit只有一个实例值()
     def testUnit : Unit = {}
     println(testUnit)
+
+
+    // Null类只有一个实例对象null 可以赋值给任意引用类型AnyRef 但是不能赋值给值类型AnyVal
+    class Cat()
+    var cat = new Cat();
+    cat = null
+    println(cat)
+
+
+
   }
 }
