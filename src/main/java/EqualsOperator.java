5ce9012e7c3b59d98c14a7c7fa97c45ca111e09d
Prometheus <598681353@qq.com>
2023-02-12 16:11:14 +0800

    add comment

--- src/main/java/EqualsOperator.java
+++ src/main/java/EqualsOperator.java
@@ -1,5 +1,8 @@
 public class EqualsOperator {
   public static void main(String[] args) {
+
+    // ==比较两个变量本身的值 即两个对象在内存中的地址
+    // equals 比较字符串中所包含的内容是否相同
     String s1 = "abc";
     String s2 = new String("abc");
     System.out.println(s1 == s2);
